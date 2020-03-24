f5123860608d280fe5df34cd1c14dee38df28017
Milos Jovanovic <jovanovicmilos.kg@gmail.com>
2020-03-24 13:38:58 +0100

    Create header component

---
+++ src/modules/dashboard/components/header.component.ts
@@ -0,0 +1,15 @@
+/**
+ * Header component
+ * Display header at application
+ *
+ * @author Milos Jovanovic
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+}
---
+++ src/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './components/header.component';
+
+@NgModule({
+    declarations: [
+        HeaderComponent
+    ],
+    imports: [
+
+    ],
+    providers: [
+
+    ],
+    exports: [
+        HeaderComponent
+    ]
+})
+export class DashboardModule {
+}
